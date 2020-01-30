847741a7e33e70d23d4589e69e2466ce4ec953e4
Anwar-alsina <anwarfahad6700@gmail.com>
2020-01-30 16:32:37 +0300

    push project to remote

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Strange' },
+  { id: 12, name: 'Black-Panther' },
+  { id: 13, name: 'BlackWidow' },
+  { id: 14, name: 'Hulk' },
+  { id: 15, name: 'Magneto' },
+  { id: 16, name: 'Spiderman' },
+  { id: 17, name: 'Cpt America' },
+  { id: 18, name: 'THANOS' },
+  { id: 19, name: 'Cpt Marvel' },
+  { id: 20, name: 'Flash' }
+];
